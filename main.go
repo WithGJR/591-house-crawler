df2726dd4c509bdfa3c3f23b544e89c06c122115
WithGJR <kevin3372000@gmail.com>
2016-06-03 00:03:32 +0800

    crawl the pages concurrently

--- main.go
+++ main.go
@@ -1,68 +1,117 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/PuerkitoBio/goquery"
+	"io/ioutil"
 	"log"
+	"strconv"
 )
 
+const (
+	rootURL string = "https://store.591.com.tw/"
+)
+
+func getPageURL(pageNumber int) string {
+	//For Taichung
+	if pageNumber == 1 {
+		return rootURL + "house-rentSale.html?storeType=1&regionid=8&search=1"
+	} else {
+		firstRow := 20 * (pageNumber - 1)
+		return rootURL + "index.php?firstRow=" + strconv.Itoa(firstRow) + "&storeType=1&regionid=8&search=1&module=house&action=rentSale"
+	}
+}
+
 type HouseInfo struct {
-	Addr string
+	Addr string `json:"addr"`
 }
 
 type Crawler struct {
-	rootURL    string
-	parameters string
-	infos      []HouseInfo
+	infos map[int][]HouseInfo
 }
 
-func (c *Crawler) SetRootURL(url string) {
-	c.rootURL = url
+type Task struct {
+	PageNumber             int
+	URLsToDetailedInfoPage []string
 }
 
-func (c *Crawler) SetParameters(parameters string) {
-	c.parameters = parameters
+type Result struct {
+	PageNumber int
+	Infos      []HouseInfo
 }
 
-func (c *Crawler) Run() {
-	doc, err := goquery.NewDocument(c.rootURL + c.parameters)
-	if err != nil {
-		log.Fatal(err)
-	}
+func (c *Crawler) handle(task Task, output chan Result) {
+	result := Result{PageNumber: task.PageNumber, Infos: make([]HouseInfo, 0)}
 
-	urlsToDetailedInfo := make([]string, 0)
-	doc.Find(".address > a").Each(func(i int, s *goquery.Selection) {
-		urlToHouse, _ := s.Attr("href")
-		urlsToDetailedInfo = append(urlsToDetailedInfo, c.rootURL+urlToHouse)
-	})
-
-	for i := 0; i < len(urlsToDetailedInfo); i++ {
-		doc, err := goquery.NewDocument(urlsToDetailedInfo[i])
+	for i := 0; i < len(task.URLsToDetailedInfoPage); i++ {
+		doc, err := goquery.NewDocument(task.URLsToDetailedInfoPage[i])
 		if err != nil {
-			//TODO
 			continue
 		}
 
 		doc.Find(".addr").Each(func(i int, s *goquery.Selection) {
 			info := HouseInfo{Addr: s.Text()}
-			c.infos = append(c.infos, info)
+			result.Infos = append(result.Infos, info)
 		})
+	}
+	output <- result
+}
+
+func (c *Crawler) Run() {
+	output := make(chan Result, 30)
+	pageNumber := 0
+
+	for {
+		pageNumber++
+		doc, err := goquery.NewDocument(getPageURL(pageNumber))
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		urlsToDetailedInfoPage := make([]string, 0)
+		doc.Find(".address > a").Each(func(i int, s *goquery.Selection) {
+			urlToHouse, _ := s.Attr("href")
+			urlsToDetailedInfoPage = append(urlsToDetailedInfoPage, rootURL+urlToHouse)
+		})
+
+		//no more pages to be crawled
+		if len(urlsToDetailedInfoPage) == 0 {
+			pageNumber--
+			break
+		} else {
+			task := Task{
+				PageNumber:             pageNumber,
+				URLsToDetailedInfoPage: urlsToDetailedInfoPage,
+			}
+			go c.handle(task, output)
+		}
+	}
+
+	for len(c.infos) != pageNumber {
+		result := <-output
+		c.infos[result.PageNumber] = result.Infos
+		fmt.Printf("Page %d is finished.\n", result.PageNumber)
 	}
 }
 
-func (c *Crawler) GetResult() []HouseInfo {
-	return c.infos
+func (c *Crawler) OutputJSONAsFile() {
+	for pageNumber, infos := range c.infos {
+		content, err := json.Marshal(infos)
+		if err != nil {
+			log.Fatal(err)
+		}
+		ioutil.WriteFile("page"+strconv.Itoa(pageNumber)+".json", content, 0644)
+	}
 }
 
 func main() {
-	crawler := &Crawler{}
-	//Taichung
-	crawler.SetRootURL("https://store.591.com.tw/")
-	crawler.SetParameters("house-rentSale.html?storeType=1&regionid=8&search=1")
-	crawler.Run()
-
-	results := crawler.GetResult()
-	for i := 0; i < len(results); i++ {
-		fmt.Printf("%d: %s\n", i+1, results[i].Addr)
+	crawler := &Crawler{
+		infos: make(map[int][]HouseInfo),
 	}
+
+	crawler.Run()
+	crawler.OutputJSONAsFile()
+
 }
